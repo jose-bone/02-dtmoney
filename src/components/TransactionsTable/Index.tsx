a231db6c04933f8f6a354c07d9f98d4b742b0f39
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-02 21:40:25 +0200

    Componente TransictionsTable

---
+++ src/components/TransactionsTable/Index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>02/12/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguer</td>
+            <td className="withdraw">- $2.000</td>
+            <td>Casa</td>
+            <td>25/11/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
