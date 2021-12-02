de6d09ad61624dcfbb8ee4afa6f8f69797e9dcbc
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-02 21:40:54 +0200

    Componentização

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,9 +2,9 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #F0F2F5;
     --red: #E52E4D;
     --blue:#5429CC;
+    --green: #33CC95;
 
     --blue-light:#6933FF;
 
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     --text-body:#969CB3;
 
     --background: #F0F2F5;
-    --shape: #ffffff;
+    --shape: #FFFFFF;
   }
 
   *{
@@ -21,20 +21,19 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  // font-size: 16px (Desktop)
   html{
     @media (max-width: 1080px){
-      font-size: 93.75%; // 15px
+      font-size: 93.75%; 
     }
 
     @media(max-width: 720px) {
-      font-size: 87.5%; // 14px
+      font-size: 87.5%; 
     }
   }
 
   body {
     background: var(---background);
-    -webkit-font-smoothing: antialiased;
+    -webkit-font-smoothing: antialised;
   }
 
   body, input, textarea, button {
